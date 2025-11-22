e65926f8897b86d78b488458044b6717dc3f9a9f
Kristien Cook <kristiencook@usf.edu>
2025-11-21 20:06:27 -0500

    Created folder and initial app.py with a welcome message.

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -1,13 +1,1 @@
 print ("Welcome to My Python Code")
-savings = input("How much money did you save for the future this month? ")
-# Attempt to collect data from the user
-try:
-    savings = float(savings)
-except ValueError:
-    print("Please enter a valid number. ")
-    savings = input("How much money did you save for the future this month? ")   
-    savings = float(savings)
-# A fail safe, incase the user types in a invalid input
-savings_year = savings * 12
-print(f"You are on pace to save ${savings_year} this year. ")
-# Math to see how much money the user will save over a years span if they save that same amount each month
